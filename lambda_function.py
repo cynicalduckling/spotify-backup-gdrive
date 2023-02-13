0afbca1e08c53fa3d185cdc1ad9a4df470aec9c3
rohithseby <48560429+rohithseby@users.noreply.github.com>
2023-02-14 00:10:29 +0530

    init

---
+++ lambda_function.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import re, pytz, io, warnings, time, os
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from datetime import datetime
+import spotify_functions as spotify
+import spotipy as spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+load_dotenv()
+IST = pytz.timezone("Asia/Kolkata")
+warnings.filterwarnings("ignore")
+start = time.time()
+
+
+def get_datetime_now():
+    return datetime.now(IST).strftime("%b %e, %Y %H:%M")
+
+
+print(f"\nStarted executing at {get_datetime_now()}")
+
+
+def drive_upload(df, drive, name):
+    file = drive.CreateFile(
+        {
+            "parents": [{"id": "1_urOyu2ViM4CRx8mHDpR1B0_qEgRKsGf"}],
+            "title": name,
+        }
+    )
+
+    df_byte = io.BytesIO()
+    df.to_excel(df_byte, index=False, sheet_name="Playlist")
+
+    file.content = df_byte
+
+    file.Upload()
+
+
+def delete_old_files_from_drive(file_list):
+    [x.Delete() for x in file_list]
+
+
+def main():
+    SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+    SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+    print("\n---> Authenticating with Spotify")
+
+    scope = [
+        "user-library-read",
+        "user-read-playback-state",
+        "app-remote-control",
+        "user-modify-playback-state",
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "playlist-modify-private",
+        "playlist-modify-public",
+        "user-top-read",
+        "user-read-recently-played",
+    ]
+
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=SPOTIFY_CLIENT_ID,
+            client_secret=SPOTIFY_CLIENT_SECRET,
+            scope=scope,
+            redirect_uri="http://127.0.0.1:9090",
+        )
+    )
+
+    time.sleep(1)
+
+    print("\n---> Authenticating with Google Drive")
+
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    query = {"q": f"title contains '_spotifyid'"}
+    files = drive.ListFile(query).GetList()
+
+    print("\n---> Getting user's playlists' info")
+
+    my_playlists = spotify.get_all_user_playlists(sp)
+
+    all_df = []
+
+    etldatetime = get_datetime_now()
+
+    time.sleep(1)
+
+    print("\n---> Preparing, procesing and uploading dataframes")
+
+    for playlist in tqdm(my_playlists):
+        df = spotify.create_playlist_dataframe(
+            spotify.get_playlist_tracks(sp, playlist["id"]), playlist
+        )
+        name = re.sub(r"[^a-zA-Z0-9 ]", "", playlist["name"])[0:30]
+        df["etl_datetime"] = etldatetime
+        artist_id_list = df["track_main_artist_id"].tolist()
+        genre_list = (
+            spotify.get_genre_list(sp, artist_id_list=artist_id_list)
+            if len(artist_id_list) > 0
+            else None
+        )
+        df["genre_list"] = list(genre_list)
+
+        drive_upload(df, drive, f"{name}_spotifyid.xlsx")
+        all_df.append(df)
+        df = None
+
+    total = (
+        pd.concat(all_df).drop_duplicates(subset=["track_id"]).reset_index(drop=True)
+    )
+
+    drive_upload(total, drive, f"total_unique_tracks_spotifyid.xlsx")
+
+    print("\n---> Deleting previous backup files")
+
+    delete_old_files_from_drive(files) if list(total.shape)[0] > 3400 else None 
+
+    end = time.time()
+
+    print(
+        f"\n---> Finished execution -- Execution time: {round((end-start)/60, 1)} minutes\n"
+    )
+
+    time.sleep(5)
+
+
+def lambda_handler(event, context):
+    main()
+
+
+lambda_handler(None, None)
---
+++ spotify_functions.py
@@ -0,0 +1,125 @@
+from dotenv import load_dotenv
+import spotipy as spotify
+from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
+import numpy
+
+load_dotenv()
+
+
+def clean_user_playlist_info(playlists):
+    return {
+        "name": playlists["name"],
+        "id": playlists["id"],
+        "visibility": "Public" if playlists["public"] else "Private",
+        "spotify_url": playlists["external_urls"]["spotify"],
+        "id": playlists["id"],
+    }
+
+
+def get_all_user_playlists(spotify_client_object):
+    final = []
+    for offset in range(0, 10000, 50):
+        result = list(
+            map(
+                clean_user_playlist_info,
+                spotify_client_object.current_user_playlists(offset=offset)["items"],
+            )
+        )
+        if len(result) == 0:
+            break
+        final = final + result
+
+    return final
+
+
+def get_playlist_tracks(spotify_client_object, playlist_id):
+    final = []
+    for offset in range(0, 10000, 100):
+        result = spotify_client_object.playlist_items(
+            playlist_id=playlist_id,
+            fields="items(track(album(artists.name, name, id, release_date, external_urls.spotify), artists(name, id, external_urls.spotify), duration_ms, id, external_urls.spotify, name, popularity))",
+            offset=offset,
+        )
+        if len(result["items"]) == 0:
+            break
+        final = final + result["items"]
+
+    return final
+
+
+def create_playlist_dataframe(track_list, playlist_info):
+    df_columns = [
+        "playlist_name",
+        "playlist_url",
+        "playlist_id",
+        "track_name",
+        "track_artists_all",
+        "track_main_artist",
+        "track_main_artist_id",
+        "track_main_artist_url",
+        "track_url",
+        "track_id",
+        "track_popularity",
+        "track_length_ms",
+        "album_name",
+        "album_release_date",
+        "album_artist",
+        "album_url",
+        "album_id",
+    ]
+    count = 0
+    df = pd.DataFrame(columns=df_columns)
+    try:
+        for entry in track_list:
+            to_append = {
+                "playlist_name": playlist_info["name"],
+                "playlist_url": playlist_info["spotify_url"],
+                "playlist_id": playlist_info["id"],
+                "track_name": entry["track"]["name"],
+                "track_artists_all": ", ".join(
+                    [x["name"] for x in entry["track"]["artists"]]
+                ),
+                "track_main_artist": entry["track"]["artists"][0]["name"],
+                "track_main_artist_id": entry["track"]["artists"][0]["id"],
+                "track_main_artist_url": entry["track"]["artists"][0]["external_urls"][
+                    "spotify"
+                ],
+                "track_url": entry["track"]["external_urls"]["spotify"],
+                "track_id": entry["track"]["id"],
+                "track_popularity": entry["track"]["popularity"],
+                "track_length_ms": entry["track"]["duration_ms"],
+                "album_name": entry["track"]["album"]["name"],
+                "album_release_date": entry["track"]["album"]["release_date"],
+                "album_artist": entry["track"]["album"]["artists"][0]["name"],
+                "album_url": entry["track"]["album"]["external_urls"]["spotify"],
+                "album_id": entry["track"]["album"]["id"],
+            }
+
+            df = df.append(to_append, ignore_index=True)
+            to_append = {}
+    except Exception as e:
+        count = count + 1
+
+    if count > 0:
+        print("exception at creating dataframe")
+        print(f"{count} tracks raised errors")
+    return df
+
+
+def get_genre_list(spotify_client_object, artist_id_list):
+    artists = []
+    try:
+        for offset in range(0, 1000, 25):
+            temp_artists = spotify_client_object.artists(
+                artist_id_list[offset : offset + 25]
+            )
+            artists = artists + temp_artists["artists"]
+            if len(artist_id_list) <= offset + 25:
+                break
+    except Exception as e:
+        print("exception at getting genres")
+        print(offset, len(artist_id_list))
+
+    final = [", ".join(x) for x in list(map(lambda x: x["genres"], artists))]
+    return final
